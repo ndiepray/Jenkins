d00a07eb92f5886855291ba8bd4b6cb216f47b48
ndiepray <86943561+ndiepray@users.noreply.github.com>
2022-02-07 16:44:48 +0900

    Update nfs-expoerter.sh

--- nfs-expoerter.sh
+++ nfs-expoerter.sh
@@ -5,7 +5,7 @@ fi
 
 if [[ ! -d $nfsdir ]]; then
   mkdir -p $nfsdir
-  echo "$nfsdir 192.168.1.0/24(rw,sync,no_root_squash)" >> /etc/exports
+  echo "$nfsdir 192.168.100.0/24(rw,sync,no_root_squash)" >> /etc/exports
   if [[ $(systemctl is-enabled nfs) -eq "disabled" ]]; then
     systemctl enable nfs
   fi
